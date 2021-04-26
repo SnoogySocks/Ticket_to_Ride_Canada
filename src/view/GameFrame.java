0f9f2f13f688e7d38feb8a15cf0ae7f0e622f50e
Felix <felixfong03@live.com>
2021-04-26 14:50:39 -0400

    commit

--- src/view/GameFrame.java
+++ src/view/GameFrame.java
@@ -13,42 +13,30 @@ public class GameFrame extends JFrame {
 
     //Constants
     private static final int WIDTH = 1920, HEIGHT = 1080;
-<<<<<<< HEAD
     
     //Panels
     private BoardPanel boardPanel;
     private ScorePanel scorePanel = new ScorePanel(1400,0,520,180);
     private CardPanel cardPanel = new CardPanel(0,900,1400,180);
-    private PlayerPanel playerPanel = new PlayerPanel(1400,180,520,900);
+    private PlayerPanel playerPanel = new PlayerPanel(1400,180,520,9
     
-    //Menubar components
-    
-    public GameFrame(City[] cities, Route[] routes){
-        boardPanel = new BoardPanel(0, 0, 1400,900, cities, routes);
-=======
-
-    //Panels
-    private BoardPanel boardPanel;
-    private ScorePanel scorePanel = new ScorePanel(1400, 0, 520, 180);
-    private CardPanel cardPanel = new CardPanel(0, 860, 1400, 180);
-    private PlayerPanel playerPanel = new PlayerPanel(1400, 180, 520, 900);
-
     //Menubar components
     private JMenuBar menuBar = new JMenuBar();
-
+    
     private JMenu fileMenu = new JMenu("File");
     private JMenuItem newMI = new JMenuItem("New");
     private JMenuItem saveMI = new JMenuItem("Save");
     private JMenuItem loadMI = new JMenuItem("Load");
     private JMenuItem exitMI = new JMenuItem("Exit");
     private JMenuItem[] fileMenuItems = {newMI, saveMI, loadMI, exitMI};
-
+    
     private JMenu helpMenu = new JMenu("Help");
     private JMenuItem helpContentsMI = new JMenuItem("Help Contents");
     private JMenuItem aboutMI = new JMenuItem("About");
     private JMenuItem[] helpMenuItems = {helpContentsMI, aboutMI};
-
+    
     public GameFrame(City[] cities, Route[] routes) {
+        
         for (JMenuItem item : fileMenuItems) {
             fileMenu.add(item);
         }
@@ -61,9 +49,8 @@ public class GameFrame extends JFrame {
         menuBar.add(helpMenu);
         setJMenuBar(menuBar);
 
-        boardPanel = new BoardPanel(cities, routes);
+        boardPanel = new BoardPanel(0, 0, 1400, 900, cities, routes);
 
->>>>>>> e739442adaf33f41fc4d8b545a2ce71f2cae0b0e
         add(boardPanel);
         setSize(WIDTH, HEIGHT);
         setLayout(null);
@@ -72,6 +59,7 @@ public class GameFrame extends JFrame {
         add(playerPanel);
         setVisible(true);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
+        
     }
 
 }
