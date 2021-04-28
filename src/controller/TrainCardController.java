cf93fe27207f926092e30072edf6e03ee691ba90
Cerena00 <47257720+Cerena00@users.noreply.github.com>
2021-04-28 13:38:45 -0400

    wee woo wee wooooooo

--- src/controller/TrainCardController.java
+++ src/controller/TrainCardController.java
@@ -1,5 +1,11 @@
 package controller;
 
+import model.TrainCard;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Stack;
+
 /**
 / This class contains methods to manage the train cards including creating the train card deck, dealing the train cards,
 / dealing a single train card to a specific player, flipping over five cards, checking for three rainbow cards showing,
@@ -9,6 +15,37 @@ package controller;
 */
 
 public class TrainCardController {
+
+    public static void createDeck(){
+
+        Stack<Integer> createDeck = new Stack<>();
+
+        ArrayList<Integer> trainCardsArray = new ArrayList<>();
+
+        //Generates 12 of each colour
+        for(int i = 1; i < 9; i++){
+
+            for( int twelveCards = 0; twelveCards < 13; twelveCards++){
+
+                trainCardsArray.add(i);
+            }
+
+        }
+
+        //Generate 14 rainbow cards and add to array list
+        for(int x = 0; x < 15; x++){
+
+            trainCardsArray.add(0);
+        }
+
+        //Shuffle array list
+        Collections.shuffle(trainCardsArray);
+
+        //Add array list into stack
+        createDeck.addAll(trainCardsArray);
+
+
+    }
 
 
 
--- src/model/TrainCard.java
+++ src/model/TrainCard.java
@@ -2,6 +2,7 @@ package model;
 
 import javax.swing.*;
 
+
 /**
  * @author Cerena
  */
